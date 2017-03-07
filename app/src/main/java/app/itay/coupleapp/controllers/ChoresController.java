b8e61e2f635762d5666c5b2a04847bc999d273ab
Iren Saturina <isaturina@zonedigital.com>
2017-03-07 13:40:52 +0200

    Open new page for : add/edit chore, add goal, add reward

---
+++ app/src/main/java/app/itay/coupleapp/controllers/ChoresController.java
@@ -0,0 +1,14 @@
+package app.itay.coupleapp.controllers;
+
+/**
+ * Created by isaturina on 3/6/17.
+ */
+
+public interface ChoresController {
+    void startChoreActivityCreate();
+    void startChoreActivityEdit(String taskName);
+    void startChoreActivityReward();
+    void startChoreActivityGoal();
+
+
+}
